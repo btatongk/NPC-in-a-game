5c57ed0cc849ee5e9c2a78b971f1df7d04873ecd
BriceTatongK <tatong.brice@gmail.com>
2025-10-22 20:34:55 +0200

    Refactor App class to retrieve OPENAI_API_KEY from application.properties and improve output messages

--- npc/src/main/java/bricetatongk/npc/App.java
+++ npc/src/main/java/bricetatongk/npc/App.java
@@ -15,44 +15,47 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( ">>>>>> Npc starts <<<<<" );
+        System.out.println( ">>>>>> Npc started <<<<<" );
 
-        String OPENAI_API_KEY = getSetting("OPENAI_API_KEY");
+        String OPENAI_API_KEY = "";
 
-        ChatModel model = OpenAiChatModel.builder().temperature(0.8).apiKey(OPENAI_API_KEY)
+        // retrieve the setting from application.properties
+        try (InputStream input = App.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("Uable to find application.properties");
+            }
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            OPENAI_API_KEY = prop.getProperty("OPENAI_API_KEY");
+
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        ChatModel model = OpenAiChatModel
+                .builder()
+                .temperature(0.8)
+                .apiKey(OPENAI_API_KEY)
 				.modelName(OpenAiChatModelName.GPT_4_O_MINI)
 				.build();
 
-        ConversationalChain cc = ConversationalChain.builder().chatModel(model).build();
+        ConversationalChain cc = ConversationalChain
+                .builder()
+                .chatModel(model)
+                .build();
+
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 System.out.print(">>");
                 if (!scanner.hasNextLine()) {
                     break;
                 }
+
                 String prompt = scanner.nextLine();
                 System.out.println("\n" + cc.execute(prompt));
             }
-        }
-    }
-
-    // Helper method to read settings from properties file
-    private static String getSetting(String key) {
-        // retrieve the setting from application.properties
-        try (InputStream input = App.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                System.out.println("Uable to find application.properties");
-                return null;
-            }
-
-            Properties prop = new Properties();
-            prop.load(input);
-
-            return prop.getProperty(key);
-
-        } catch (IOException ex){
-            ex.printStackTrace();
-            return null;
         }
     }
 }
